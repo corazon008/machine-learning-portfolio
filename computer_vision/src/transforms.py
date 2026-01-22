09cf3373c27af8c2171434255d60fefb3be4b99c
corazon <99136079+corazon008@users.noreply.github.com>
2026-01-22 17:35:10 +0100

    cnn: feat: Add mlflow to emotions

--- computer_vision/src/transforms.py
+++ computer_vision/src/transforms.py
@@ -2,9 +2,12 @@
 from torchvision import transforms
 
 
-def get_transform(img_size: int, mean: list, std: list) -> transforms.Compose:
-    return transforms.Compose([
+def get_transform(img_size: int, mean: list, std: list, gray_scale:bool=False) -> transforms.Compose:
+    t= transforms.Compose([
         transforms.Resize((img_size, img_size)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std)
+        transforms.Normalize(mean=mean, std=std),
     ])
+    if gray_scale:
+        t.transforms.insert(len(t.transforms), transforms.Grayscale(num_output_channels=1))
+    return t

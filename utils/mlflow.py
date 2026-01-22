74f4b52a13d5c09bbf77a4b5ce274594ee86c219
corazon <99136079+corazon008@users.noreply.github.com>
2026-01-22 23:29:07 +0100

    cnn: refactor mlflow usage

--- utils/mlflow.py
+++ utils/mlflow.py
@@ -1,48 +1,96 @@
-import mlflow
 import os
+
+import mlflow
+import requests
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import GridSearchCV
+from skorch import NeuralNet
+from skorch.helper import SliceDataset
+from torch.utils.data.dataset import Dataset
+
 from utils.helper import find_project_root
 
-MLFLOW_PID = -1
+PORT = 5000
+MLFLOW_TRACKING_URI = "http://localhost:{}".format(PORT)
+
 
 def start_mlflow_server():
     """
     Start the MLflow server if it is not already running.
     :return:
     """
-    # Navigate to root directory and start MLflow server
-    global MLFLOW_PID
-    if MLFLOW_PID != -1 or is_mlflow_server_running():
+    # Navigate to root directory and start MLflow serve
+    if is_mlflow_server_running():
         return  # Server already running
     os.chdir(find_project_root())
-    mlflow_cmd = "mlflow server --port 5000"
-    MLFLOW_PID = os.system(mlflow_cmd)
+    mlflow_cmd = "mlflow server --port {}".format(PORT)
+    os.system(mlflow_cmd)
 
-def is_mlflow_server_running()->bool:
-    """
-    Check if the MLflow server is running by attempting to connect to it.
-    :return:
-    bool: True if the server is running, False otherwise.
-    """
+
+def is_mlflow_server_running() -> bool:
     try:
-        mlflow.set_tracking_uri("http://localhost:5000")
-        mlflow.get_experiment_by_name("test_experiment")
-        return True
-    except Exception:
+        r = requests.get(f"{MLFLOW_TRACKING_URI}/", timeout=2)
+    except requests.ConnectTimeout:
         return False
+    except requests.ConnectionError:
+        return False
+    if r.status_code != 200:
+        return False
+    return True
 
-def stop_mlflow_server():
-    """
-    Stop the MLflow server if it is running.
-    :return:
-    """
-    global MLFLOW_PID
-    if MLFLOW_PID != -1:
-        os.kill(MLFLOW_PID, 9)
-        MLFLOW_PID = -1
 
 def set_mlflow_tracking_uri():
     # Set MLflow tracking URI to local server
-    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+
+
+def log_run(model: BaseEstimator | GridSearchCV, X_train: Dataset | SliceDataset, y_train,
+            X_test: Dataset | SliceDataset, y_test, params=None, run_name=None) -> NeuralNet:
+    """
+    Log a training run to MLflow.
+    :param model:
+    :param X_train:
+    :param y_train:
+    :param X_test:
+    :param y_test:
+    :param params:
+    :return: The trained model
+    """
+    run_name = run_name if run_name else model.__class__.__name__
+    with mlflow.start_run(run_name=run_name):
+        if isinstance(params, dict):
+            mlflow.log_params(params)
+        else:
+            mlflow.log_param("params", str(params))
+
+        model.fit(X_train, y_train)
+        if isinstance(model, GridSearchCV):
+            mlflow.log_param('best_params', model.best_params_)
+            mlflow.log_metric('best_cv_accuracy', float(model.best_score_) * 100)
+
+            print("Best parameters found: ", model.best_params_)
+            print("Best cross-validation accuracy: ", model.best_score_ * 100)
+
+            model = model.best_estimator_
+        else:
+            pass
+
+        mlflow.pytorch.log_model(model.module_, name=run_name)
+
+        test_accuracy = model.score(X_test, y_test) * 100
+        print("Test set accuracy: ", test_accuracy)
+
+        mlflow.log_metric('test_accuracy', float(test_accuracy))
+
+        # log history metrics
+        for epoch, row in enumerate(model.history):
+            if 'train_loss' in row:
+                mlflow.log_metric('train_loss', float(row['train_loss']), step=epoch)
+            if 'valid_loss' in row:
+                mlflow.log_metric('val_loss', float(row['valid_loss']), step=epoch)
+
+        return model
+
 
 if __name__ == "__main__":
     start_mlflow_server()

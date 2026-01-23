dba6a26ab2a401ed9ae7f33973fa69094ecb36b1
corazon <99136079+corazon008@users.noreply.github.com>
2026-01-23 16:54:59 +0100

    reorder files
    all import are broken for now

---
+++ src/computer_vision/models/baseline.py
@@ -0,0 +1,19 @@
+import torch.nn.functional as F
+from torch import nn
+
+
+# Baseline model for CNN
+class BaselineModel(nn.Module):
+    def __init__(self, input_dim=32 * 32 * 3, hidden_dim=98, output_dim=10, dropout=0.5, ):
+        super(BaselineModel, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+
+        self.hidden = nn.Linear(input_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, X, **kwargs):
+        X = X.reshape(-1, self.hidden.in_features)
+        X = F.relu(self.hidden(X))
+        X = self.dropout(X)
+        X = F.softmax(self.output(X), dim=-1)
+        return X
